b813b368a9fbc29c5a6281a3d6421e4987ad7292
mogmoug <mogmoug123@outlook.com>
2023-01-26 21:31:15 +0800

    重构成了Vue3

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,33 +3,15 @@
     windows_subsystem = "windows"
 )]
 
-use std::process::*;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/commands
-
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn xdg_open(args: &str) {
-    let mut binding = Command::new("xdg-open");
-    let command = binding.arg(args);
-    match &command.output(){
-        Ok(o) => println!("{}",String::from_utf8_lossy(&o.stdout)),
-        Err(_) => eprintln!("Failed to open"),
-    }
-}
-
-#[tauri::command]
-fn system_exec(command: &str, args: &str) {
-    let mut binding = Command::new(command);
-    let command = binding.arg(args);
-    match &command.output(){
-        Ok(o) => println!("{}",String::from_utf8_lossy(&o.stdout)),
-        Err(_) => eprintln!("Failed to execute"),
-    }
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![xdg_open, system_exec])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
